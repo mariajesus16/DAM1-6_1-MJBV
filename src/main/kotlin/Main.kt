fff57767b53dc807c24044c2f4c1d1b3a72075c3
mariajesus16 <mjborrego@iesrafaelalberti.es>
2022-01-17 21:39:34 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+open class Planta() {
+}
+
+class Cactus() : Planta() {
+    fun
+}
+
+class Girasol(var sol: Boolean) : Planta() {
+    fun heliotropismo(sol: Boolean) {
+        if()
+    }
+}
+
+
+fun main() {
+}
